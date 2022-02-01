647340ca57670fb4a4a4c69f656494fb7122312a
Rajiv-Kinyanjui <peterkinyanjui226@gmail.com>
2022-02-01 10:30:21 +0300

    elif statements

--- input_demo.py
+++ input_demo.py
@@ -1,6 +1,10 @@
-height = 54 #This unit is in inches
+height = 68 #This unit is in inches
 
 if height > 70:
     print("You are really tall")
+
+elif height > 60:
+    print("You are of average height")
+    
 else:
     print("You are really short")
